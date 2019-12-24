703c3496c33af4c13f64fca7ff2c3340e36307b1
Ari Wasch <ariwasch@gmail.com>
2019-12-24 09:20:05 -0500

    Initial Commit

---
+++ NFC Digital Keypad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NFC Digital Keypad
+//
+//  Created by Ari Wasch on 12/24/19.
+//  Copyright © 2019 Ari Wasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NFC Digital KeypadTests/NFC_Digital_KeypadTests.swift
@@ -0,0 +1,34 @@
+//
+//  NFC_Digital_KeypadTests.swift
+//  NFC Digital KeypadTests
+//
+//  Created by Ari Wasch on 12/24/19.
+//  Copyright © 2019 Ari Wasch. All rights reserved.
+//
+
+import XCTest
+@testable import NFC_Digital_Keypad
+
+class NFC_Digital_KeypadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
